e829e3b9b79ee68a081073d1a3547800680bb88a
wcolina <walker.cbdiv@gmail.com>
2018-04-24 02:11:47 -0400

    first commit

---
+++ src/main/java/com/tr/testrepo2/Testrepo2Application.java
@@ -0,0 +1,12 @@
+package com.tr.testrepo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testrepo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testrepo2Application.class, args);
+    }
+}
